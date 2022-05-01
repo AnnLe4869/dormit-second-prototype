b0fcafea9c9b2265f9511f3e2f04ce62c84ebeaa
yih365 <68409635+gigi365@users.noreply.github.com>
2022-05-01 00:24:19 -0700

    account

--- src/routes/account/Account.js
+++ src/routes/account/Account.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Account() {
+
+  // example student
+  let student = {
+    'name': 'Alex',
+    'college': 'Revelle',
+    'building' : 'building name',
+    'floor': 1,
+    'apartment': 12,
+    'phone': '8585342230',
+    'email': 'aturner0@ucsd.edu'
+  }
+
   return (
     <div>Account</div>
   )
