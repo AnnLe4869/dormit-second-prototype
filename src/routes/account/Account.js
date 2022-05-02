2a1b4d4113c01c1a7fadc5ce1ddb066d59093fa6
yih365 <68409635+gigi365@users.noreply.github.com>
2022-05-01 21:47:37 -0700

    update

--- src/routes/account/Account.js
+++ src/routes/account/Account.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Account.css';
 
 export default function Account() {
 
@@ -11,10 +12,55 @@ export default function Account() {
     'floor': 1,
     'apartment': 12,
     'phone': '8585342230',
-    'email': 'aturner0@ucsd.edu'
+    'email': 'aturner0@ucsd.edu',
+    'pronouns': 'they/them'
+  }
+
+  const toSurvey = () => {
+
   }
 
   return (
-    <div>Account</div>
+    <div>
+      <div className='intro'>
+        <img src="imagelink.com/img/13718623" id='pfp'></img>
+
+        <div className='text'>
+          <h4>{'Hi, ' + student['name'] + ' (' +student['pronouns']+ ')'}</h4>
+          <h5>{'UCSD - ' + student['college'] + ' student'}</h5>
+          <a href="logout">Log Out</a>
+        </div>
+      </div>
+
+      <div className='accountPage'>
+        <div className='boxes'>
+          <h1 className='title'>Your Impact</h1>
+          <p>{student['name'] + ', you are 1 of (X) ' + student['college'] + ' students who prevented Xg carbon emissions and helped #X Rushers(s)!'}</p>
+          <p>Have some time to fill a quick optional survey? Share the link with a friend and get a shout out on Instagram!</p>
+          <button id='surveyBtn' onClick={toSurvey}>Take me to survey</button>
+        </div>
+
+        <div className='boxes'>
+          <h1 className='title'>Profile</h1>
+
+        </div>
+
+        <div className='boxes'>
+          <h1 className='title'>Payment Methods</h1>
+
+        </div>
+
+        <div className='boxes'>
+          <h1 className='title'>Refer a friend</h1>
+
+        </div>
+
+        <div className='boxes'>
+          <h1 className='title'>Contact Us</h1>
+
+        </div>
+      </div>
+
+    </div>
   )
 }
