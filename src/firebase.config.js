ea2fbe02701c71054cb230766f1b125dae51f683
Anh Le <kunquan2345@gmail.com>
2022-06-28 14:32:25 -0700

    Update the config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,22 +2,13 @@ const config = {
   // copy the credential from the firebase project
   // it should look something like this
 
-  // apiKey: "AIcadcdasSF_Ng3Gsg7v_L6casfecdhzqC89U90",
-  // authDomain: "evernote-e077e.firebaseapp.com",
-  // databaseURL: "https://evernote-e077e.firebaseio.com",
-  // projectId: "evernote-e077e",
-  // storageBucket: "evernote-e077e.appspot.com",
-  // messagingSenderId: "333330020976",
-  // appId: "1:33cds30020976:web:54cf65cda4e6db7b3b52b5",
-  // measurementId: "G-9YCRCSDNSW8",
-
-  apiKey: "AIzaSyDXvP6R2_ALQhNB_s8_JoseuM265ySVi0g",
-  authDomain: "test-d66bc.firebaseapp.com",
-  projectId: "test-d66bc",
-  storageBucket: "test-d66bc.appspot.com",
-  messagingSenderId: "630921852577",
-  appId: "1:630921852577:web:35cc6dfd56ce3923f3e2e0",
-  measurementId: "G-FJ8LN4VTBG",
+  apiKey: "AIzaSyAJFe5JBX1JXD2ygq4ryDamTavbO2e9Po4",
+  authDomain: "dormit-second-prototype.firebaseapp.com",
+  projectId: "dormit-second-prototype",
+  storageBucket: "dormit-second-prototype.appspot.com",
+  messagingSenderId: "451270285728",
+  appId: "1:451270285728:web:9ad8890ea66d822a176cc5",
+  measurementId: "G-LW1R7N143W",
 };
 
 export default config;
