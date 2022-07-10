bc7db426e17d6910d12cbe97a9cb63d70ad5cc6f
Anh Le <kunquan2345@gmail.com>
2022-07-10 04:34:29 -0700

    Set up email authentication
    - When user want to sign in using email authentication, we send code to their email
    - No test has been done

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
   ],
   plugins: ["@typescript-eslint", "import"],
   rules: {
-    quotes: ["error", "double"],
+    quotes: ["error", "double", { allowTemplateLiterals: true }],
     "import/no-unresolved": 0,
   },
 };
--- functions/src/config.ts
+++ functions/src/config.ts
@@ -1,3 +1,4 @@
 export default {
-  stripeSecretKey: process.env.STRIPE_API_KEY || "api secret key",
+  stripeSecretKey: process.env.STRIPE_API_KEY || "stripe api key error",
+  otpSecret: process.env.OTP_SECRET || "otp secret error",
 };
