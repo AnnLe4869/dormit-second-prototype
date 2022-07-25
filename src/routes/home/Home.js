dfa736b9e409ec44e0c76759470a9403424ff117
Anh Le <kunquan2345@gmail.com>
2022-07-25 03:28:01 -0700

    Rename and restructure files

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,53 +1,30 @@
-import { useState, useRef } from "react";
-import { ProductContext } from "../../context/product/product-context";
-import {
-  useSignUp,
-  useSignOut,
-  useCheckAuthenticationStatus,
-  useEmailSignIn,
-} from "../../context/user/auth-handler";
-import { useCheckout } from "../../context/user/checkout-handler";
+import { useRef } from "react";
 
 /*
  * Style Sheet
  */
-import './Home.css';
+import "./Home.css";
+import { useProducts } from "../../context/product/product-handler";
 
 /*
  * Imported Components
  */
-import Header from './Header';
-import ViewCart from '../../shared/viewcart/ViewCart';
-import Taskbar from '../../shared/taskbar/Taskbar';
-import ItemEntry from '../../shared/itementry/ItemEntry';
-import Category from './Category';
+import ItemEntry from "../../shared/item-entry/ItemEntry";
+import BottomNav from "../../shared/bottom-nav/BottomNav";
+import ViewCart from "../../shared/view-cart/ViewCart";
+import Category from "./Category";
+import Header from "./Header";
 
 /*
  * Imported Assets
  */
-import todaysSpecialIcon from '../../assets/Home/todays-special-icon.svg';
-import apple from '../../assets/apple.png';
+import apple from "../../assets/apple.png";
+import todaysSpecialIcon from "../../assets/Home/todays-special-icon.svg";
 
 export default function HomePage() {
-  // const status = useCheckAuthenticationStatus();
-  // const signUp = useSignUp();
-  // const signOut = useSignOut();
-  // const checkout = useCheckout();
+  const products = useProducts();
 
-  // const { sendCode, authenticateUser } = useEmailSignIn();
-
-  // const [code, setCode] = useState("");
-
-  // const handleChange = (event) => {
-  //   setCode(event.target.value);
-  // };
-
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   authenticateUser("asa@gmail.com", code);
-  //   console.log(code);
-  // };
-
+  console.log(products);
   /*
    * Mock lists
    */
@@ -55,7 +32,7 @@ export default function HomePage() {
     <ItemEntry id="apple" name="Apple" image={apple} price="Price" stock={2} />,
     <ItemEntry id="apple" name="Apple" image={apple} price="Price" stock={0} />,
     <ItemEntry id="apple" name="Apple" image={apple} price="Price" stock={1} />,
-    <ItemEntry id="apple" name="Apple" image={apple} price="Price" stock={5} />
+    <ItemEntry id="apple" name="Apple" image={apple} price="Price" stock={5} />,
   ];
 
   const mockForYouItems = [
@@ -66,25 +43,24 @@ export default function HomePage() {
     <ItemEntry id="apple" name="Apple" image={apple} price="Price" stock={2} />,
     <ItemEntry id="apple" name="Apple" image={apple} price="Price" stock={0} />,
     <ItemEntry id="apple" name="Apple" image={apple} price="Price" stock={1} />,
-    <ItemEntry id="apple" name="Apple" image={apple} price="Price" stock={5} />
+    <ItemEntry id="apple" name="Apple" image={apple} price="Price" stock={5} />,
   ];
 
   const mockDealItems = [
-    <ItemEntry price={"Price"} image={apple} dealPrice={"Price"}/>,
-    <ItemEntry price={"Price"} image={apple} dealPrice={"Price"}/>,
-    <ItemEntry price={"Price"} image={apple} dealPrice={"Price"}/>,
-    <ItemEntry price={"Price"} image={apple} dealPrice={"Price"}/>
-
-  ]
+    <ItemEntry price={"Price"} image={apple} dealPrice={"Price"} />,
+    <ItemEntry price={"Price"} image={apple} dealPrice={"Price"} />,
+    <ItemEntry price={"Price"} image={apple} dealPrice={"Price"} />,
+    <ItemEntry price={"Price"} image={apple} dealPrice={"Price"} />,
+  ];
 
   const mockCategoryItems = [
-    <ItemEntry price={"Price"} image={apple}/>,
-    <ItemEntry price={"Price"} image={apple}/>,
-    <ItemEntry price={"Price"} image={apple}/>,
-    <ItemEntry price={"Price"} image={apple}/>,
-    <ItemEntry price={"Price"} image={apple}/>,
-    <ItemEntry price={"Price"} image={apple}/>
-  ]
+    <ItemEntry price={"Price"} image={apple} />,
+    <ItemEntry price={"Price"} image={apple} />,
+    <ItemEntry price={"Price"} image={apple} />,
+    <ItemEntry price={"Price"} image={apple} />,
+    <ItemEntry price={"Price"} image={apple} />,
+    <ItemEntry price={"Price"} image={apple} />,
+  ];
 
   //useRef() constants for page categories
   const chipsRef = useRef(null);
@@ -93,7 +69,7 @@ export default function HomePage() {
   const drinksRef = useRef(null);
   const sweetsRef = useRef(null);
   const iceCreamRef = useRef(null);
-  const readyToEatRef = useRef(null);  
+  const readyToEatRef = useRef(null);
   const refArray = [
     chipsRef,
     snacksRef,
@@ -101,15 +77,14 @@ export default function HomePage() {
     drinksRef,
     sweetsRef,
     iceCreamRef,
-    readyToEatRef
+    readyToEatRef,
   ];
-
 
   /*
    * onClick event handler to scroll to food category
    */
   function scrollToCategory(index) {
-    refArray[index].current.scrollIntoView({behavior: 'smooth'});
+    refArray[index].current.scrollIntoView({ behavior: "smooth" });
   }
 
   return (
@@ -117,16 +92,15 @@ export default function HomePage() {
       <Header />
       <div className="homeContainer">
         <div className="homeContent">
-
           {/* Navbar for Food Categories */}
           <section className="homeCategoryNav">
-            <button onClick={e => scrollToCategory(0)}>Chips</button>
-            <button onClick={e => scrollToCategory(1)}>Snacks</button>
-            <button onClick={e => scrollToCategory(2)}>Energy</button>
-            <button onClick={e => scrollToCategory(3)}>Drinks</button>
-            <button onClick={e => scrollToCategory(4)}>Sweets</button>
-            <button onClick={e => scrollToCategory(5)}>Ice Cream</button>
-            <button onClick={e => scrollToCategory(6)}>Ready-to-Eat</button>
+            <button onClick={(e) => scrollToCategory(0)}>Chips</button>
+            <button onClick={(e) => scrollToCategory(1)}>Snacks</button>
+            <button onClick={(e) => scrollToCategory(2)}>Energy</button>
+            <button onClick={(e) => scrollToCategory(3)}>Drinks</button>
+            <button onClick={(e) => scrollToCategory(4)}>Sweets</button>
+            <button onClick={(e) => scrollToCategory(5)}>Ice Cream</button>
+            <button onClick={(e) => scrollToCategory(6)}>Ready-to-Eat</button>
           </section>
 
           {/* Bulletin */}
@@ -138,13 +112,17 @@ export default function HomePage() {
 
           {/* Today's Special */}
           <section className="todaysSpecial">
-            <img src={todaysSpecialIcon} alt="Today's Special" className="todaysSpecialIcon"/>
+            <img
+              src={todaysSpecialIcon}
+              alt="Today's Special"
+              className="todaysSpecialIcon"
+            />
             <h2>Today's Special</h2>
             <h3>Get it while it's hot!</h3>
 
             <ul className="bigItemList">
-              {mockSpecialItems.map(item => {
-                return <li>{item}</li>
+              {mockSpecialItems.map((item) => {
+                return <li>{item}</li>;
               })}
             </ul>
           </section>
@@ -153,8 +131,8 @@ export default function HomePage() {
           <section className="smallItemSection">
             <h3>For You</h3>
             <ul className="smallItemList">
-              {mockForYouItems.map(item => {
-                return <li>{item}</li>
+              {mockForYouItems.map((item) => {
+                return <li>{item}</li>;
               })}
             </ul>
           </section>
@@ -163,8 +141,8 @@ export default function HomePage() {
           <section className="smallItemSection">
             <h3>Trending</h3>
             <ul className="smallItemList">
-              {mockForYouItems.map(item => {
-                return <li>{item}</li>
+              {mockForYouItems.map((item) => {
+                return <li>{item}</li>;
               })}
             </ul>
           </section>
@@ -173,109 +151,102 @@ export default function HomePage() {
           <section className="smallItemSection">
             <h3>Deals</h3>
             <ul className="smallItemList">
-              {mockDealItems.map(item => {
-                return <li>{item}</li>
+              {mockDealItems.map((item) => {
+                return <li>{item}</li>;
               })}
             </ul>
           </section>
 
           {/* Food Categories */}
           <section className="categorySection">
-
             {/* Chips Section */}
             <div id="chipsSection" ref={chipsRef}>
-              <Category 
-                name="Chips" 
+              <Category
+                name="Chips"
                 itemList={mockCategoryItems}
-                style={{ borderImageSource: 'linear-gradient(90.28deg, #F0786F 0%, #FABA68 100%)' }} 
+                style={{
+                  borderImageSource:
+                    "linear-gradient(90.28deg, #F0786F 0%, #FABA68 100%)",
+                }}
               />
             </div>
 
             {/* Snacks Section */}
             <div id="snacksSection" ref={snacksRef}>
-              <Category 
-                name="Snacks" 
+              <Category
+                name="Snacks"
                 itemList={mockCategoryItems}
-                style={{ borderImageSource: 'linear-gradient(90.28deg, #FABA68 0%, #FFEB83 100%)' }} 
+                style={{
+                  borderImageSource:
+                    "linear-gradient(90.28deg, #FABA68 0%, #FFEB83 100%)",
+                }}
               />
             </div>
 
             {/* Energy Section */}
             <div id="energySection" ref={energyRef}>
-              <Category 
-                name="Energy" 
+              <Category
+                name="Energy"
                 itemList={mockCategoryItems}
-                style={{ borderImageSource: 'linear-gradient(90.28deg, #FFEB83 0%, #CDF888 100%)' }} 
+                style={{
+                  borderImageSource:
+                    "linear-gradient(90.28deg, #FFEB83 0%, #CDF888 100%)",
+                }}
               />
             </div>
 
             {/* Drinks Section */}
             <div id="drinksSection" ref={drinksRef}>
-              <Category 
-                name="Drinks" 
+              <Category
+                name="Drinks"
                 itemList={mockCategoryItems}
-                style={{ borderImageSource: 'linear-gradient(90.28deg, #88F8CF 0%, #88BBF8 100%)' }} 
+                style={{
+                  borderImageSource:
+                    "linear-gradient(90.28deg, #88F8CF 0%, #88BBF8 100%)",
+                }}
               />
             </div>
 
             {/* Sweets Section */}
             <div id="sweetsSection" ref={sweetsRef}>
-              <Category 
-                name="Sweets" 
+              <Category
+                name="Sweets"
                 itemList={mockCategoryItems}
-                style={{ borderImageSource: 'linear-gradient(90.28deg, #945B46 0%, #DEAD98 100%)' }} 
+                style={{
+                  borderImageSource:
+                    "linear-gradient(90.28deg, #945B46 0%, #DEAD98 100%)",
+                }}
               />
             </div>
 
             {/* Ice Cream Section */}
             <div id="icecreamSection" ref={iceCreamRef}>
-              <Category 
-                name="Ice Cream" 
+              <Category
+                name="Ice Cream"
                 itemList={mockCategoryItems}
-                style={{ borderImageSource: 'linear-gradient(90.28deg, #DB88F8 0%, #F88888 100%)' }} 
+                style={{
+                  borderImageSource:
+                    "linear-gradient(90.28deg, #DB88F8 0%, #F88888 100%)",
+                }}
               />
             </div>
 
             {/* Ready-to-Eat Section */}
             <div id="readytoeatSection" ref={readyToEatRef}>
-              <Category 
-                name="Ready-to-Eat" 
+              <Category
+                name="Ready-to-Eat"
                 itemList={mockCategoryItems}
-                style={{ borderImageSource: 'linear-gradient(90.28deg, #DC4A4A 0%, #ECA8A8 100%)' }} 
+                style={{
+                  borderImageSource:
+                    "linear-gradient(90.28deg, #DC4A4A 0%, #ECA8A8 100%)",
+                }}
               />
             </div>
-
           </section>
         </div>
       </div>
-      <ViewCart numItems="X" totalAmount="X.XX"/>
-      <Taskbar currentPage="home"/>
+      <ViewCart numItems="X" totalAmount="X.XX" />
+      <BottomNav currentPage="home" />
     </>
-
-    // <main>
-    //   {status ? (
-    //     <>
-    //       <button onClick={signOut}>Sign out</button>
-    //       <button onClick={checkout}>Checkout</button>
-    //     </>
-    //   ) : (
-    //     <>
-    //       <button
-    //         onClick={() => {
-    //           sendCode("asa@gmail.com");
-    //           console.log("code is sent");
-    //         }}
-    //       >
-    //         Send code to email
-    //       </button>
-    //       <form onSubmit={handleSubmit}>
-    //         <label form="code">Enter code</label>
-    //         <input type="text" id="code" onChange={handleChange} />
-    //         <button>Submit</button>
-    //       </form>
-    //       <button onClick={signUp}>Sign up</button>
-    //     </>
-    //   )}
-    // </main>
   );
 }
---
+++ src/shared/item-entry/ItemEntry.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import "./ItemEntry.css";
+
+/*
+ * Imported Assets
+ */
+import greenCheck from "../../assets/ItemEntry/green-check.svg";
+import purplePlus from "../../assets/ItemEntry/purple-plus.svg";
+
+const ItemEntry = ({ id, name, image, price, dealPrice, stock }) => {
+  //useState() constant for Plus/Check icon
+  const [inCart, setInCart] = useState(false);
+
+  let added = inCart;
+
+  /*
+   * Function which toggles Plus/Check icon flag.
+   */
+  function toggleCorner() {
+    added = !added;
+    setInCart(added);
+  }
+
+  /*
+   * onClick listener which is called when an item is clicked
+   */
+  function toggleImage() {
+    alert("Apple clicked");
+  }
+
+  /*
+   * Function which returns description of item stock
+   */
+  function checkStock(stock) {
+    if (stock === 0) {
+      console.log("Out of stock");
+      return <p>Out of stock</p>;
+    } else if (stock === 1) {
+      console.log("Only 1 left!");
+      return <p className="purpleFont">Only 1 left!</p>;
+    } else if (stock < 4) {
+      console.log("Low in stock");
+      return <p className="purpleFont">Low in stock</p>;
+    }
+    return;
+  }
+
+  return (
+    <div className="itemContainer">
+      {/* Corner button (Plus/Check) */}
+      <button className="itemCorner" onClick={toggleCorner}>
+        {inCart ? (
+          <img src={greenCheck} alt="Added Item" />
+        ) : (
+          <img src={purplePlus} alt="Add Item" />
+        )}
+      </button>
+
+      {/* Item image */}
+      <button className="itemImage" onClick={toggleImage}>
+        <img src={image} alt="Item" />
+      </button>
+
+      {/* Item Info (Price, Stock) */}
+      <section className="itemInfo" style={stock ? {} : { color: "#686868" }}>
+        {dealPrice ? (
+          <>
+            <p className="purpleFont">${dealPrice}</p>
+            <p className="strikeThrough">${price}</p>
+          </>
+        ) : (
+          <p>${price}</p>
+        )}
+
+        <div className="stockInfo">{checkStock(stock)}</div>
+      </section>
+    </div>
+  );
+};
+
+export default ItemEntry;
