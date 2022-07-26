f7a1a0b7d6a53a8bf436fdfd27e87c0c31339726
mohakvni <68292499+mohakvni@users.noreply.github.com>
2022-07-25 23:52:31 -0700

    CSS Refactor and Changed Cart to camelCase

--- src/routes/checkout/Cart.js
+++ src/routes/checkout/Cart.js
@@ -15,10 +15,10 @@ const Cart = () => {
   return (
     <div>
       <main>
-        <div className={styles.Cartbuttondiv}>
-          <button className = {styles.Cartbutton} onClick={()=> setShow(true)}>
-            View Cart
+        <div className={styles.cartButtonDiv}>
+          <button className = {styles.cartButton} onClick={()=> setShow(true)}>
             <div className={styles.price}>$XX.XX</div>
+            View Cart
             <div className={styles.count}>X Items</div>
           </button>
         </div>
@@ -33,7 +33,7 @@ const Cart = () => {
         <Stack gap={4}>
             <div style={{"margin":"auto"}}>
                 <Offcanvas.Title>
-                    <div className={styles.headertext}>Order Details</div>
+                    <div className={styles.headerText}>Order Details</div>
                 </Offcanvas.Title>
             </div>
             <div>
@@ -44,41 +44,41 @@ const Cart = () => {
 
 
         <Offcanvas.Body>
-          <div className={styles.cart_item}><Cart_item /></div>
+          <div className={styles.cartItem}><Cart_item /></div>
           <br></br>
-          <div className={styles.cart_item}><Cart_item /></div>
+          <div className={styles.cartItem}><Cart_item /></div>
           <br></br>
-          <div className={styles.cart_item}><Cart_item /></div>
+          <div className={styles.cartItem}><Cart_item /></div>
           <br></br>
         </Offcanvas.Body>
+
         <Modal.Footer style={{'white-space' : 'normal'}}>
           <Stack gap={1}>
           <Container style = {{'max-width':'384px'}}>
             <Row style = {{'margin-bottom':'13px'}}>
-              <Col><div className={styles.footer_subs}>Subtotal</div></Col>
-              <Col style={{'text-align': 'right'}}><div className={styles.footer_subs}>$X.XX</div></Col>
+              <Col><div className={styles.footerSubs}>Subtotal</div></Col>
+              <Col style={{'text-align': 'right'}}><div className={styles.footerSubs}>$X.XX</div></Col>
             </Row>
             <Row style = {{'margin-bottom':'13px'}}>
-              <Col><div className={styles.footer_subs}>Tax</div></Col>
-              <Col style={{'text-align': 'right'}}><div className={styles.footer_subs}>$X.XX</div></Col>
+              <Col><div className={styles.footerSubs}>Tax</div></Col>
+              <Col style={{'text-align': 'right'}}><div className={styles.footerSubs}>$X.XX</div></Col>
             </Row>
             <Row style = {{'margin-bottom':'13px'}}>
-              <Col><div className={styles.footer_subs}>Delivery</div></Col>
-              <Col style={{'text-align': 'right'}}><div className={styles.footer_subs}>$X.XX</div></Col>
+              <Col><div className={styles.footerSubs}>Delivery</div></Col>
+              <Col style={{'text-align': 'right'}}><div className={styles.footerSubs}>$X.XX</div></Col>
             </Row>
             <Row style = {{'margin-bottom':'13px'}}>
-              <Col><div className={styles.footer_total}>Total</div></Col>
-              <Col style={{'text-align': 'right'}}><div className={styles.footer_total}>$X.XX</div></Col>
+              <Col><div className={styles.footerTotal}>Total</div></Col>
+              <Col style={{'text-align': 'right'}}><div className={styles.footerTotal}>$X.XX</div></Col>
             </Row>
           </Container>
-          
-          <div className={styles.footer_button_div}>
-            <button className={styles.footer_button} onClick={() => {navigate("/order/details")}}>
-              <div className={styles.ft_text_small2}>X Items</div>
-              <div className={styles.ft_text_big}>Review Order</div>
-              <div className={styles.ft_text_small}>$X.XX</div>
-            </button>
-          </div>
+
+          <button className={styles.footerButton} onClick={() => {navigate("/order/details")}}>
+              <div className={styles.footerButtonItemCount}>X Items</div>
+              <div className={styles.footerButtonMain}>Review Order</div>
+              <div className={styles.footerButtonPrice}>$X.XX</div>
+          </button>
+
           </Stack>
         </Modal.Footer>
       </Offcanvas>
