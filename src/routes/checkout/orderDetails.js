8797cc342c5785bc732686acaf49fa05f7b292e6
mohakvni <68292499+mohakvni@users.noreply.github.com>
2022-07-26 00:07:18 -0700

    Further CSS Refactor and switch to camelCase

--- src/routes/checkout/orderDetails.js
+++ src/routes/checkout/orderDetails.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Button, Stack, ButtonGroup, Form, FormGroup, Navbar} from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom';
-import styles from "../checkout/Order_Details.module.css"
+import styles from "../checkout/orderDetails.module.css"
 
 
 const Review_Cart = () => {
@@ -21,7 +21,7 @@ const Review_Cart = () => {
 
 
         <div className={styles.header}>
-            <div className={styles.headertext}>
+            <div className={styles.headerText}>
             Order Details
             </div>
         </div>
@@ -29,21 +29,21 @@ const Review_Cart = () => {
             <hr style={{"margin-top":"-5px", "width":"100%"}}></hr>
         </div>
         <Form>
-            <div className={styles.addressform}>
+            <div className={styles.addressForm}>
                 <div className={styles.title}>Address</div>
                 <div className={styles.inputs}>
-                    <FormGroup style={{"margin-right":"50px"}} className={styles.input_div}>
-                        <Form.Label bsPrefix={styles.input_title}>UCSD Building</Form.Label>
+                    <FormGroup style={{"margin-right":"50px"}} className={styles.inputDiv}>
+                        <Form.Label bsPrefix={styles.inputTitle}>UCSD Building</Form.Label>
                         <Form.Control type="input" placeholder="Building Name" required/>
                     </FormGroup>
-                    <FormGroup className={styles.input_div}>
-                        <Form.Label bsPrefix={styles.input_title}>Floor / Apt #</Form.Label>
+                    <FormGroup className={styles.inputDiv}>
+                        <Form.Label bsPrefix={styles.inputTitle}>Floor / Apt #</Form.Label>
                         <Form.Control type="input" placeholder="1/12" required />
                     </FormGroup>
                 </div>
 
-                <FormGroup className={styles.notes_div}>
-                        <Form.Label bsPrefix={styles.input_title}>Delivery Notes</Form.Label>
+                <FormGroup className={styles.notesDiv}>
+                        <Form.Label bsPrefix={styles.inputTitle}>Delivery Notes</Form.Label>
                         <Form.Control type="input" placeholder="Notes for your Rusher" className={styles.notes}/>
                 </FormGroup>
                 
@@ -56,7 +56,7 @@ const Review_Cart = () => {
                         <div className={styles.payment}>
                             <img src="./public/wallet.svg" style={{'margin-right':'15px'}}></img>
                             **** **** **** 1234
-                            <button className={styles.nextbutton}>
+                            <button className={styles.nextButton}>
                                 <img src='./next.svg'></img>
                             </button>
                         </div>
@@ -65,9 +65,9 @@ const Review_Cart = () => {
                     <Stack>
                         <div>Replacement Items</div>
                         <ButtonGroup bsPrefix={styles.replacement} required>
-                            <Button className={styles.replacement_button} type="button"><div className={styles.tip}>Pick for me</div></Button>
-                            <Button className={styles.replacement_button} type="button"><div className={styles.tip}>Call me</div></Button>
-                            <Button className={styles.replacement_button} type="button"><div className={styles.tip}>Refund me</div></Button>
+                            <Button className={styles.replacementButton} type="button"><div className={styles.tip}>Pick for me</div></Button>
+                            <Button className={styles.replacementButton} type="button"><div className={styles.tip}>Call me</div></Button>
+                            <Button className={styles.replacementButton} type="button"><div className={styles.tip}>Refund me</div></Button>
                         </ButtonGroup>
                     </Stack>
                 </div>
@@ -75,19 +75,19 @@ const Review_Cart = () => {
                     <Stack>
                         <div>Tip For Rusher</div>
                         <ButtonGroup>
-                            <button className={styles.tipbutton} type="button"><div className={styles.tip}>$1.00</div></button>
-                            <button className={styles.tipbutton} type="button"><div className={styles.tip}>$1.50</div></button>
-                            <button className={styles.tipbutton} type="button"><div className={styles.tip}>$2.00</div></button>
-                            <button className={styles.tipbutton} type="button"><div className={styles.tip}>Custom</div></button>
+                            <button className={styles.tipButton} type="button"><div className={styles.tip}>$1.00</div></button>
+                            <button className={styles.tipButton} type="button"><div className={styles.tip}>$1.50</div></button>
+                            <button className={styles.tipButton} type="button"><div className={styles.tip}>$2.00</div></button>
+                            <button className={styles.tipButton} type="button"><div className={styles.tip}>Custom</div></button>
                         </ButtonGroup>
                     </Stack>
                 </div>
             </Stack>
             <div style={{"text-align":"center"}}>
-                <button className={styles.placeorderbutton} type="submit" onClick={()=>{navigate("/order/complete")}}>
-                    <div className={styles.placeorder}>X Items</div>
-                    <div className={styles.placeorder}>Place Order</div>
-                    <div className={styles.placeorder}>$X.XX</div>
+                <button className={styles.placeOrderButton} type="submit" onClick={()=>{navigate("/order/complete")}}>
+                    <div className={styles.placeOrder}>X Items</div>
+                    <div className={styles.placeOrder}>Place Order</div>
+                    <div className={styles.placeOrder}>$X.XX</div>
                 </button>
             </div>
         </Form>
