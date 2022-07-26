a9ac169608c16f26581dc553a1f365a33001a828
Anh Le <kunquan2345@gmail.com>
2022-07-25 23:31:16 -0700

    Adjust the naming and spacing

--- src/routes/auth/LostAccess/LostAccess.js
+++ src/routes/auth/LostAccess/LostAccess.js
@@ -1,29 +1,31 @@
-import React, {useRef} from 'react'
-import styles from '../Auth.module.css'
-import callIcon from '../../../mock_data/images/callVector.png'
+import React, { useRef } from "react";
+import styles from "../Auth.module.css";
+import callIcon from "../../../mock_data/images/callVector.png";
 
-
-function PhoneCheck({nextStep, lostAccess}) {
-
+function PhoneCheck({ nextStep, lostAccess }) {
   return (
-    <div className={styles.centering} >
+    <div className={styles.centering}>
+      <img src={callIcon} className={styles.callIcon} />
 
-        <img src={callIcon} className={styles.callIcon} />
+      <h1>Lost access to your phone?</h1>
 
-        <h1>Lost access to your phone?</h1>
+      <p>
+        <span className={styles.purpleText}>Enter your email address</span>{" "}
+        associated with your account
+      </p>
 
-        <p>
-          <span className={styles.purpleText}>Enter your email address</span> associated with your account
-        </p>
-        
-        <input placeholder='johndoe@example.com' className={styles.inputPhone} type='text' maxLength='10'></input>
-       
-        <button onClick={nextStep} className={styles.confirmButton}>
-          Confirm
-        </button>
+      <input
+        placeholder="johndoe@example.com"
+        className={styles.inputPhone}
+        type="text"
+        maxLength="10"
+      ></input>
 
+      <button onClick={nextStep} className={styles.confirmButton}>
+        Confirm
+      </button>
     </div>
-  )
+  );
 }
 
-export default PhoneCheck
+export default PhoneCheck;
--- src/routes/auth/PhoneAuth/PhoneAuth.js
+++ src/routes/auth/PhoneAuth/PhoneAuth.js
@@ -1,49 +1,62 @@
-import React, {useEffect, useRef, useState} from 'react'
-import styles from '../Auth.module.css'
-import callIcon from '../../../mock_data/images/callVector.png'
+import React, { useEffect, useRef, useState } from "react";
+import styles from "../Auth.module.css";
+import callIcon from "../../../mock_data/images/callVector.png";
 
+const authCode = new Array(4).fill(0);
 
-const authCode = new Array(4).fill(0)
+function PhoneAuth({ nextStep }) {
+  const [currentInput, setCurrentInput] = useState(0);
+  let inputRef = useRef(null);
+  let buttonRef = useRef();
 
-function Phoneauth({nextStep}) {
-    const [currentInput, setCurrentInput] = useState(0)
-    let inputRef = useRef(null)
-    let buttonRef = useRef()
-    
+  const handleOnchange = (index) => {
+    authCode.splice(index, 1, inputRef.current.value);
 
-    const handleOnchange = (index) => {
-        authCode.splice(index, 1, inputRef.current.value)
-
-        if(currentInput < 3) {
-            inputRef.current?.focus()
-            return setCurrentInput(currentInput + 1)
-        }
-
-        inputRef.current?.blur();
-        buttonRef.current.click();
-        return
+    if (currentInput < 3) {
+      inputRef.current?.focus();
+      return setCurrentInput(currentInput + 1);
     }
 
-    useEffect(() => {
-        inputRef.current?.focus()
-        
-    })
-    
+    inputRef.current?.blur();
+    buttonRef.current.click();
+    return;
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  });
+
   return (
     <div className={styles.centering}>
-        <img src={callIcon} className={styles.callIcon} />
-        <h1>Verification</h1>
-        <p>We sent you an <span className={styles.purpleText}>SMS code to (xxx) xxx-xxxx</span> </p>
-        <div className={styles.squareInputLayout}>
-            {
-                authCode.map((val, index) => {
-                    return <input key={index} onChange={() => handleOnchange(index)} type='number' ref={index === currentInput ? inputRef : null} className={styles.inputVerify} placeholder={val} ></input>
-                })
-            }
-        </div>
-        <button ref={buttonRef} onClick={nextStep} className={styles.confirmButton}>Confirm</button>
+      <img src={callIcon} className={styles.callIcon} />
+      <h1>Verification</h1>
+      <p>
+        We sent you an{" "}
+        <span className={styles.purpleText}>SMS code to (xxx) xxx-xxxx</span>{" "}
+      </p>
+      <div className={styles.squareInputLayout}>
+        {authCode.map((val, index) => {
+          return (
+            <input
+              key={index}
+              onChange={() => handleOnchange(index)}
+              type="number"
+              ref={index === currentInput ? inputRef : null}
+              className={styles.inputVerify}
+              placeholder={val}
+            ></input>
+          );
+        })}
+      </div>
+      <button
+        ref={buttonRef}
+        onClick={nextStep}
+        className={styles.confirmButton}
+      >
+        Confirm
+      </button>
     </div>
-  )
+  );
 }
 
-export default Phoneauth
+export default PhoneAuth;
