1b374da0f28283af1999079c3589b388914f7c26
Anh Le <kunquan2345@gmail.com>
2022-07-26 17:28:28 -0700

    Clean up user data when user sign out

---
+++ src/helper/removeCartFromLocalStorage.js
@@ -0,0 +1,15 @@
+/**
+ * remove user's data from local storage
+ * the only data that associate with user stored in localStorage is cart's info
+ */
+export function removeUserDataFromLocalStorage() {
+  const storage = {};
+
+  const keys = Object.keys(localStorage).filter(
+    (key) => !["products", "timestamp"].includes(key)
+  );
+
+  keys.forEach((key) => {
+    storage[key] = localStorage.removeItem(key);
+  });
+}
