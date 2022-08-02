32814b10b2d5bb2f750646ec956a97fae09b28c9
Darief Maes <dariefmaes@gmail.com>
2022-08-03 01:18:19 +0200

    Added new designs + MUI integration

--- src/routes/order/OrderView/OrderView.js
+++ src/routes/order/OrderView/OrderView.js
@@ -1,41 +1,74 @@
-import React from 'react'
-import styles from '../Order.module.css'
-import apple from '../../../mock_data/images/apple.jpg'
+import React from "react";
+import styles from "../Order.module.css";
+import apple from "../../../mock_data/images/apple.jpg";
+import msgIcon from "../../../mock_data/images/msgIcon.png";
+import PhoneIcon from "../../../mock_data/images/PhoneIcon.png";
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
 
-function OrderView({order}) {
-
-    const totalProducts = order.length
+function OrderView({ order }) {
+  const totalProducts = order.length;
 
   return (
     <div className={styles.orderContainer}>
-            <div className={styles.imageContainer}>
-                {order.map((product, index) => index < 3 ? <div><img src={apple} /></div> : index < 4 ? <span>+{totalProducts-3} items</span> : false)}
+      <div className={styles.imageContainer}>
+        {order.products.map((product, index) =>
+          index < 3 ? (
+            <div key={index}>
+              <img src={apple} />
             </div>
-            <div className={styles.textContainer}>
-                <div className={styles.orderInfo}>
-                    {/* Mapping ove items */}
-                    <h3>
-                    {
-                        //mapping over products from props
-                        order.map((product, index) => {
-                            return <span>{(index ? ', ' : '') + product.name}</span>
-                        })
-                    }
-                    </h3>
+          ) : index < 4 ? (
+            <span>+{totalProducts - 3} items</span>
+          ) : (
+            false
+          )
+        )}
+      </div>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <div className={styles.orderInfo}>
+          {/* Mapping ove items */}
+          <Typography variant="h6" fontWeight={"600"}>
+            {
+              //mapping over products from props
+              order.products.map((product, index) => {
+                return (
+                  <span key={index}>{(index ? ", " : "") + product.name}</span>
+                );
+              })
+            }
+          </Typography>
 
-                    <p>March 22 at 08:05 pm</p>
-                    <p>$11.55 • {order.length} item{order.length > 1 ? "s" : ""}</p>
-                    <p>Complete</p>              
-                </div>
-            
+          <p>March 22 at 08:05 pm</p>
+          <p>
+            $11.55 • {order.length} item{order.length > 1 ? "s" : ""}
+          </p>
+          <p>Complete</p>
         </div>
-        <div className={styles.orderAction}>
-                {/* Reorder and report */}
-                <button className={styles.colorPurple}>Reorder</button>
-                <button className={styles.colorRed}>Report</button>
-            </div>
+      </Box>
+      {order.status === "completed" ? (
+        <div className={styles.completedAction}>
+          {/* Reorder and report */}
+          <button className={styles.colorPurple}>Reorder</button>
+          <button className={styles.colorRed}>Report</button>
+        </div>
+      ) : (
+        <div className={styles.currentAction}>
+          {/* Reorder and report */}
+          <Typography variant="body1" lineHeight={"20px"} fontWeight={"normal"}>
+            On the way • 4 min <br /> Brandon K.
+          </Typography>
+          <Box sx={{ display: "flex", gap: 1 }}>
+            <button className={styles.currentBtn}>
+              <img src={PhoneIcon} />
+            </button>
+            <button className={styles.currentBtn}>
+              <img src={msgIcon} />
+            </button>
+          </Box>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default OrderView
+export default OrderView;
