9454b9cc1f4f72eebbffaebfe5bca15cdc965151
mohakvni <68292499+mohakvni@users.noreply.github.com>
2022-08-01 18:03:58 -0700

    Pre material things

---
+++ src/routes/checkout/Alerts/closedStore.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Alert } from "react-bootstrap";
+import styles from "../checkout/Cart.module.css";
+
+export const closedStore = () => {
+  return (
+    <>
+      <Alert key="danger" variant="danger" className={styles.closedStoreAlert}>
+        Store is closed
+      </Alert>
+    </>
+  );
+};
---
+++ src/routes/checkout/Alerts/outOfStock.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Alert } from "react-bootstrap";
+import styles from "../checkout/Cart.module.css";
+
+export const outOfStock = () => {
+  return (
+    <>
+      <Alert key="danger" variant="danger" className={styles.outOfStockAlert}>
+        <CloseButton
+          style={{ position: "relative", top: "10%", right: "5%" }}
+          variant="white"
+        />
+        [ItemName] Out of Stock
+      </Alert>
+    </>
+  );
+};
