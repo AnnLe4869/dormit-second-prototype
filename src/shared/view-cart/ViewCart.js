f6b2be3fc12557ae36a55ebfb57a9814454b01ea
Ben Lee <dbinlee3@gmail.com>
2022-08-02 21:19:40 -0700

    Homepage MUI Conversion - WIP

--- src/shared/view-cart/ViewCart.js
+++ src/shared/view-cart/ViewCart.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import ViewCartCSS from './ViewCart.module.css';
+import React from 'react'
+import { viewCartStyles } from '../../muiStyles.js';
 
-const ViewCart = ({numItems, totalAmount}) => {
-    return (
-        <button className={ViewCartCSS.viewCart}>
+/*
+ * Material-UI Imports
+ */
+import Button from "@mui/material/Button";
+import Grid from '@mui/material/Grid';
+import Typography from "@mui/material/Typography";
 
-            <div className={ViewCartCSS.items}>{numItems} items</div>
-            <h4>View Cart</h4>
-            <div className={ViewCartCSS.totalAmount}>${totalAmount}</div>
-
-        </button>
-    )
+function ViewCart({numItems, totalAmount}) {
+  return (
+    <Button sx={viewCartStyles.viewCartContainer}>
+        <Grid container spacing={0} sx={viewCartStyles.viewCartGrid}>
+            <Grid item xs={3.5} sx={viewCartStyles.viewCartText}>
+                <Typography variant="h5" color="secondary.main" fontWeight={600} marginBottom="6px">
+                    {numItems} Items
+                </Typography>
+            </Grid>
+            <Grid item xs={5} sx={viewCartStyles.viewCartText}>
+                <Typography variant="h3" color="secondary.main" fontWeight={600}>
+                    View Cart
+                </Typography>
+            </Grid>
+            <Grid item xs={3.5} sx={viewCartStyles.viewCartText}>
+                <Typography variant="h5" color="secondary.main" fontWeight={600} marginBottom="6px">
+                    ${totalAmount}
+                </Typography>
+            </Grid>
+        </Grid>
+    </Button>
+  )
 }
 
 export default ViewCart
