e0f5c77d3cda1268e12866f7c1ac446e421efcc4
Hyunjo <hyl130@ucsd.edu>
2022-08-03 11:44:20 -0700

    Changed reactjs-popup to MUI Dialog

--- src/routes/category/supplies/suppliesTemplate/SuppliesTemplate.js
+++ src/routes/category/supplies/suppliesTemplate/SuppliesTemplate.js
@@ -2,9 +2,8 @@ import React , {useState} from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./SuppliesTemplate.module.css";
 import Item from '../../item/Item.js'
-import Popup from 'reactjs-popup';
-import "./SuppliesPopUp.css"
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import Dialog from '@mui/material/Dialog';
 
 const SuppliesTemplate = (props) => {
 
@@ -25,9 +24,17 @@ const SuppliesTemplate = (props) => {
       </div>
       <div className={styles.price}>$Price</div>
       </div>
-      <Popup open={show} className="supplies-popup" modal nested lockScroll="true">
-        <Item onClose={setShow} link="https://firebasestorage.googleapis.com/v0/b/dormit-second-prototype.appspot.com/o/products%2Fapple.jpg?alt=media&token=372a4141-e0e3-4521-bf51-604ed8622430" />
-      </Popup>
+
+      <div>
+        <Dialog
+          fullScreen
+          open={show}
+          onClose={setShow}
+        >
+          <Item onClose={setShow} link="https://firebasestorage.googleapis.com/v0/b/dormit-second-prototype.appspot.com/o/products%2Fapple.jpg?alt=media&token=372a4141-e0e3-4521-bf51-604ed8622430" />
+        </Dialog>
+      </div>
+
     </>
   );
 };
