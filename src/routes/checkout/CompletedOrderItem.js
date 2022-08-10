3604784a2626cca6f62d0fbd6f1375c6657d521c
mohakvni <68292499+mohakvni@users.noreply.github.com>
2022-08-09 17:53:51 -0700

    Ran Prettier

--- src/routes/checkout/CompletedOrderItem.js
+++ src/routes/checkout/CompletedOrderItem.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import styles from "./CompletedOrderItem.module.css"
+import React from "react";
+import styles from "./CompletedOrderItem.module.css";
 
-const CompletedOrderItem = ({id, name, desc, price, quantity, pic}) => {
+const CompletedOrderItem = ({ id, name, desc, price, quantity, pic }) => {
   return (
     <>
-    <div className={styles.Border}>
+      <div className={styles.Border}>
         <div className={styles.picture}>
-            <img style={{'border':'none', 'width':'85px', 'height':'85px'}} src = {pic}></img>
-            <div className={styles.Count}>{quantity}</div>
+          <img
+            style={{ border: "none", width: "85px", height: "85px" }}
+            src={pic}
+          ></img>
+          <div className={styles.Count}>{quantity}</div>
         </div>
         <div className={styles.details}>
-            <div className={styles.name}>{name}</div>
-            <div className={styles.desc}>{desc}</div>
-            <div className={styles.price}>${price * quantity}</div>
+          <div className={styles.name}>{name}</div>
+          <div className={styles.desc}>{desc}</div>
+          <div className={styles.price}>${price * quantity}</div>
         </div>
-        
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default CompletedOrderItem
+export default CompletedOrderItem;
--- src/routes/checkout/OrderDetails.js
+++ src/routes/checkout/OrderDetails.js
@@ -1,117 +1,140 @@
-import React from 'react'
-import BottomNav from "../../shared/bottom-nav/BottomNav"
-import styles from "./OrderDetails.module.css"
-import { TextField, Divider, Button, ButtonGroup } from '@mui/material'
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import CreditCardIcon from '@mui/icons-material/CreditCard';
-import AppleIcon from '@mui/icons-material/Apple';
+import React from "react";
+import BottomNav from "../../shared/bottom-nav/BottomNav";
+import styles from "./OrderDetails.module.css";
+import { TextField, Divider, Button, ButtonGroup } from "@mui/material";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import AppleIcon from "@mui/icons-material/Apple";
 import apple from "../../mock_data/images/apple.jpg";
 
-
 const details = () => {
-    return (
-        <div className={styles.box}>
-        <div className = {styles.outerPicture}>
-            <img className={styles.images} src={apple}></img>
-            <img className={styles.images} src={apple}></img>
+  return (
+    <div className={styles.box}>
+      <div className={styles.outerPicture}>
+        <img className={styles.images} src={apple}></img>
+        <img className={styles.images} src={apple}></img>
+      </div>
+      <div className={styles.details}>
+        <div className={styles.row}>Item Name, Item Name</div>
+        <div className={styles.row}>
+          <p>Item Count</p>
+          <p>2</p>
         </div>
-        <div className={styles.details}>
-            <div className={styles.row}>Item Name, Item Name</div>
-            <div className={styles.row}>
-                <p>Item Count</p>
-                <p>2</p>
-            </div>
-            <div className={styles.row}>
-                <p>Total</p>
-                <p>$11.75</p>
-            </div>
+        <div className={styles.row}>
+          <p>Total</p>
+          <p>$11.75</p>
         </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 const OrderDetails = () => {
-
   return (
     <>
-    <header>
-        <button className={styles.back}><ArrowBackIosIcon fontSize='large'/></button>
+      <header>
+        <button className={styles.back}>
+          <ArrowBackIosIcon fontSize="large" />
+        </button>
         <p> Order Details </p>
-    </header>
+      </header>
 
-    <Divider />
-    
-    <div className={styles.body}>
+      <Divider />
 
+      <div className={styles.body}>
         <div className={styles.paymentBox}>
-            <div className={styles.title}>Order Summary</div>
-            <div>{details()}</div>
+          <div className={styles.title}>Order Summary</div>
+          <div>{details()}</div>
         </div>
 
         <div className={styles.addressBox}>
-            <div className={styles.title}>Address</div>
-            <div className={styles.input}>
-                <div>UCSD Building</div>
-                <TextField id="outlined-basic" label="UCSD Building" variant="outlined" required sx={{"height":"inherit", "width":"343px"}}/>
-            </div>
-            <div className={styles.input}>
-                <div>Floor / Apartment #</div>
-                <TextField id="outlined-basic" label="1/12" required variant="outlined" sx={{"height":"inherit", "width":"343px"}}/>
-            </div>
-            <div className={styles.input}>
-                <div>Notes for Rusher</div>
-                <TextField id="outlined-basic" label="Notes" variant="outlined" sx={{"height":"inherit", "width":"343px"}}/>
-            </div>
+          <div className={styles.title}>Address</div>
+          <div className={styles.input}>
+            <div>UCSD Building</div>
+            <TextField
+              id="outlined-basic"
+              label="UCSD Building"
+              variant="outlined"
+              required
+              sx={{ height: "inherit", width: "343px" }}
+            />
+          </div>
+          <div className={styles.input}>
+            <div>Floor / Apartment #</div>
+            <TextField
+              id="outlined-basic"
+              label="1/12"
+              required
+              variant="outlined"
+              sx={{ height: "inherit", width: "343px" }}
+            />
+          </div>
+          <div className={styles.input}>
+            <div>Notes for Rusher</div>
+            <TextField
+              id="outlined-basic"
+              label="Notes"
+              variant="outlined"
+              sx={{ height: "inherit", width: "343px" }}
+            />
+          </div>
         </div>
-        
+
         <div className={styles.paymentBox}>
-            <div className={styles.title}>Payment</div>
-            <div className={styles.input}>
-                <div>Discover Card 4414</div>
-                <button style={{"border" : "0", "background" : "none", "cursor":"pointer"}}><NavigateNextIcon /></button>
-            </div>
+          <div className={styles.title}>Payment</div>
+          <div className={styles.input}>
+            <div>Discover Card 4414</div>
+            <button
+              style={{ border: "0", background: "none", cursor: "pointer" }}
+            >
+              <NavigateNextIcon />
+            </button>
+          </div>
         </div>
 
         <div className={styles.rusherTips}>
-            <div className={styles.title}>Rusher Tip</div>
-            <ButtonGroup variant='contained' sx={{"border-radius" : "20px"}}>
-                <button className={styles.tipButton}>$1.50</button>
-                <button className={styles.tipButton}>$2.00</button>
-                <button className={styles.tipButton}>$2.50</button>
-                <button className={styles.tipButton}>Other</button>
-            </ButtonGroup>
+          <div className={styles.title}>Rusher Tip</div>
+          <ButtonGroup variant="contained" sx={{ "border-radius": "20px" }}>
+            <button className={styles.tipButton}>$1.50</button>
+            <button className={styles.tipButton}>$2.00</button>
+            <button className={styles.tipButton}>$2.50</button>
+            <button className={styles.tipButton}>Other</button>
+          </ButtonGroup>
         </div>
 
         <div className={styles.replacement}>
-            <div className={styles.title}>Replacement Items</div>
-            <ButtonGroup variant='contained' sx={{"border-radius" : "20px"}}>
-                <button className={styles.replaceButton}>Pick for me</button>
-                <button className={styles.replaceButton}>Call me</button>
-                <button className={styles.replaceButton}>Refund me</button>
-            </ButtonGroup>
+          <div className={styles.title}>Replacement Items</div>
+          <ButtonGroup variant="contained" sx={{ "border-radius": "20px" }}>
+            <button className={styles.replaceButton}>Pick for me</button>
+            <button className={styles.replaceButton}>Call me</button>
+            <button className={styles.replaceButton}>Refund me</button>
+          </ButtonGroup>
         </div>
 
         <button className={styles.placeOrder}>
-            <CreditCardIcon fontSize='large'/>
-            Place Order
+          <CreditCardIcon fontSize="large" />
+          Place Order
         </button>
 
-        <button className={styles.placeOrder} style={{"background" : "black"}}>
-            <AppleIcon fontSize='large'/>
-            Place Order
+        <button className={styles.placeOrder} style={{ background: "black" }}>
+          <AppleIcon fontSize="large" />
+          Place Order
         </button>
 
-        <button className={styles.placeOrder} style={{"background" : "#008CFF", "column-gap":"10px"}}>
-            <img src='./venmo.svg'></img>
-            Place Order
+        <button
+          className={styles.placeOrder}
+          style={{ background: "#008CFF", "column-gap": "10px" }}
+        >
+          <img src="./venmo.svg"></img>
+          Place Order
         </button>
-
-    </div>
-    <footer><BottomNav /></footer>
-    
+      </div>
+      <footer>
+        <BottomNav />
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default OrderDetails
+export default OrderDetails;
