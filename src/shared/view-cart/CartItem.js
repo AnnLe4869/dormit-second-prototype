526aac6ca5da55f4a31f70f0a4460ff9906895aa
Darief Maes <dariefmaes@gmail.com>
2022-08-30 14:05:54 +0200

    Cart drawer

---
+++ src/shared/view-cart/CartItem.js
@@ -0,0 +1,201 @@
+import React, { useEffect, useState } from "react";
+import styles from "./CartItem.module.css";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import { Box } from "@mui/system";
+import { Button, Typography } from "@mui/material";
+
+const Cart_item = ({ id, name, desc, quantity, photo, price }) => {
+  const [item, setitem] = useState(true);
+  const [quan, setquantity] = useState(quantity);
+
+  const toggleDel = () => {
+    setitem(false);
+  };
+  const increase = () => {
+    setquantity(quan + 1);
+  };
+  const decrease = () => {
+    setquantity(quan - 1);
+  };
+
+  useEffect(() => {
+    if (quan <= 0) {
+      setquantity(0);
+      setTimeout(() => {
+        setitem(false);
+      }, 600);
+    }
+  }, [quan]);
+
+  const Counter = () => {
+    return (
+      <Box
+        sx={{
+          maxWidth: "120px",
+          width: "100%",
+          height: "44px",
+          display: "flex",
+          justifyContent: "center",
+          flex: "row",
+          background: "#eeeeee",
+        }}
+      >
+        <Button
+          sx={{
+            minWidth: "0",
+            minHeight: "0",
+            width: "40%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            border: "none",
+          }}
+          onClick={increase}
+        >
+          <ArrowDropUpIcon />
+        </Button>
+        <Typography
+          variant="body1"
+          sx={{
+            height: "inherit",
+            width: "10%",
+            fontFamily: "BlinkMacSystemFont",
+            fontStyle: "normal",
+            fontWeight: "400",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            textAlign: "center",
+          }}
+        >
+          {quan}
+        </Typography>
+        <Button
+          sx={{
+            minWidth: "0",
+            minHeight: "0",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            border: "none",
+            width: "40%",
+          }}
+          onClick={decrease}
+        >
+          <ArrowDropDownIcon />
+        </Button>
+      </Box>
+    );
+  };
+
+  return (
+    <>
+      {item ? (
+        <Box
+          sx={{
+            width: "100%",
+            height: "170px",
+            background: "#ffffff",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            marginBottom: "10px",
+            paddingY: "10px",
+          }}
+        >
+          <Box
+            sx={{
+              height: "100%",
+              display: "flex",
+              flexDirection: "row",
+              gap: "10px",
+              alignItems: "center",
+            }}
+          >
+            <img src={photo} className={styles.image} />
+            <Box
+              sx={{
+                alignItems: "start",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                height: "100%",
+              }}
+            >
+              <Box>
+                <Typography
+                  variant="body1"
+                  sx={{
+                    fontFamily: "BlinkMacSystemFont",
+                    fontStyle: "normal",
+                    fontWeight: "400",
+                    fontSize: "27px",
+                    lineHeight: "32px",
+                    color: "#000000",
+                  }}
+                >
+                  {name}
+                </Typography>
+                <Typography
+                  variant="body1"
+                  sx={{
+                    fontFamily: "BlinkMacSystemFont",
+                    fontStyle: "normal",
+                    fontWeight: "400",
+                    fontSize: "22px",
+                    lineHeight: "26px",
+                    color: "#686868",
+                    marginTop: "10px",
+                    marginBottom: "10px",
+                  }}
+                >
+                  {desc}
+                </Typography>
+              </Box>
+              <DeleteOutlineIcon
+                onClick={toggleDel}
+                sx={{
+                  backgroundColor: "#ffffff",
+                  border: "none",
+                  alignSelf: "flex-start",
+                  marginTop: "20px",
+                  color: "#686868",
+                  ":hover": {
+                    cursor: "pointer",
+                  },
+                }}
+                fontSize="large"
+              />
+            </Box>
+          </Box>
+          <Box
+            sx={{
+              alignItems: "end",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+              height: "100%",
+              width: "20%",
+            }}
+          >
+            <Typography
+              fontFamily="BlinkMacSystemFont"
+              fontSize="27px"
+              variant="body1"
+            >
+              ${price * quantity}
+            </Typography>
+            <Counter />
+          </Box>
+        </Box>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default Cart_item;
--- src/shared/view-cart/ViewCart.js
+++ src/shared/view-cart/ViewCart.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { viewCartStyles, headers } from "./muiStyles.js";
+import Cart from "./Cart";
 
 /*
  * Material-UI Imports
@@ -7,28 +8,51 @@ import { viewCartStyles, headers } from "./muiStyles.js";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import { Drawer } from "@mui/material";
 
 function ViewCart({ numItems, totalAmount }) {
+  const [drawerState, setDrawerState] = useState(false);
+
+  const handleDrawerClose = () => {
+    setDrawerState(false);
+  };
+
+  const handleDrawerOpen = () => {
+    setDrawerState(true);
+  };
   return (
-    <Button sx={viewCartStyles.viewCartContainer}>
-      <Grid container spacing={0} sx={viewCartStyles.viewCartGrid}>
-        <Grid item xs={3.5} sx={viewCartStyles.viewCartText}>
-          <Typography sx={headers.header5} fontWeight={600} marginBottom="6px">
-            {numItems} Items
-          </Typography>
+    <>
+      <Button sx={viewCartStyles.viewCartContainer} onClick={handleDrawerOpen}>
+        <Grid container spacing={0} sx={viewCartStyles.viewCartGrid}>
+          <Grid item xs={3.5} sx={viewCartStyles.viewCartText}>
+            <Typography
+              sx={headers.header5}
+              fontWeight={600}
+              marginBottom="6px"
+            >
+              {numItems} Items
+            </Typography>
+          </Grid>
+          <Grid item xs={5} sx={viewCartStyles.viewCartText}>
+            <Typography sx={headers.header3} fontWeight={600}>
+              View Cart
+            </Typography>
+          </Grid>
+          <Grid item xs={3.5} sx={viewCartStyles.viewCartText}>
+            <Typography
+              sx={headers.header5}
+              fontWeight={600}
+              marginBottom="6px"
+            >
+              ${totalAmount}
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={5} sx={viewCartStyles.viewCartText}>
-          <Typography sx={headers.header3} fontWeight={600}>
-            View Cart
-          </Typography>
-        </Grid>
-        <Grid item xs={3.5} sx={viewCartStyles.viewCartText}>
-          <Typography sx={headers.header5} fontWeight={600} marginBottom="6px">
-            ${totalAmount}
-          </Typography>
-        </Grid>
-      </Grid>
-    </Button>
+      </Button>
+      <Drawer anchor="bottom" open={drawerState} variant="temporary">
+        <Cart handleDrawerClose={handleDrawerClose} />
+      </Drawer>
+    </>
   );
 }
 
