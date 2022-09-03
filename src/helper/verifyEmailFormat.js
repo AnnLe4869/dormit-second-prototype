710dd4b232f592f3a3177656e03c3fafd690489a
Anh Le <kunquan2345@gmail.com>
2022-09-03 03:37:46 -0700

    Set up the auth/email routes
    - Now user can enter their email. This will perform some simple email validation
      before sending the code to the email

---
+++ src/helper/verifyEmailFormat.js
@@ -0,0 +1,6 @@
+export function verifyEmailFormat(text) {
+  const re =
+    /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+
+  return text.match(re);
+}
