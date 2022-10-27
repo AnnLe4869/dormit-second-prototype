7ae11d58438f2b42425b0075f0d97d3c56d499f4
utku <mattaktas94@gmail.com>
2022-10-27 19:24:11 -0400

    phone Verification page view complated

--- supervisor/src/App.js
+++ supervisor/src/App.js
@@ -1,13 +1,15 @@
 import LeftPanel from "./components/LeftPanel/LeftPanel";
+import PhoneVerification from "./components/PhoneVerification/PhoneVerification";
 import RightPanel from "./components/RightPanel/RightPanel";
 import RusherOnlineRequest from "./components/RusherOnlineRequest/RusherOnlineRequest";
 
 function App() {
   return (
     <div className="App">
-      <LeftPanel />
-      <RightPanel />
+      {/* <LeftPanel />
+      <RightPanel /> */}
       {/* <RusherOnlineRequest /> */}
+      <PhoneVerification />
     </div>
   );
 }
