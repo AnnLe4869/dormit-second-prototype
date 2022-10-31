e7ee7ed4a3398ed53f399adf75beaa763d5a6012
Reem Kandil <93699385+reemkandil@users.noreply.github.com>
2022-10-31 08:56:55 +0200

    Creating custom hover effect for left panel orders

--- supervisor/src/components/LeftPanel/components/LeftPanel__Order.js
+++ supervisor/src/components/LeftPanel/components/LeftPanel__Order.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { useState } from "react";
+import { Divider } from "@mui/material";
+import { mockProductsSupervisor } from "../../../mock_data_supervisor/data/mockDataSupervisor";
 
 const LeftPanel__Order = ({ order }) => {
+  const [isShown, setIsShown] = useState(false);
+
   return (
     <div className="leftPanel__Order">
       <div className="namePhone">
@@ -9,6 +14,29 @@ const LeftPanel__Order = ({ order }) => {
       </div>
       <div className="adress">{order.adress}</div>
       <div className="eta">ETA {order.eta} min</div>
+
+      {/* Custom Hover Effect */}
+      <div className="order-info-text"
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}>
+        Order Info
+      </div>
+      {/* order details */}
+      {isShown && (
+        <div className="order-details">
+          <div className="namePhone">
+            <span className="name">{order.clientName}</span> - {order.orderNo} (
+            {order.itemAmount} items)
+          </div>
+          <div>
+            <p>Items</p>
+            </div>
+          <Divider />
+          <div className="adress">{order.adress}</div>
+          <Divider />
+          <div className="eta">ETA {order.eta} min</div>
+        </div>
+      )}
     </div>
   );
 };
